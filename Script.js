85f73663dea41794fa4b2e98e023f239e89baeaf
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-22 11:36:12 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ function uploadImage() {
     const file = fileInput.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function (e) {
+        reader.onload = function (e) { 
             const image = document.getElementById('uploadedImage');   
             image.src = e.target.result;
             image.style.display = 'block';
